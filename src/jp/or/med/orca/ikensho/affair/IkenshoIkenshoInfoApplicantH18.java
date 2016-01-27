4776905b04e49024edb93cac5028bfaf15bd3994
yoshida <yoshida@cvs.orca.med.or.jp>
2016-01-27 09:57:47 +0900

    released ver 3.2.2

--- src/jp/or/med/orca/ikensho/affair/IkenshoIkenshoInfoApplicantH18.java
+++ src/jp/or/med/orca/ikensho/affair/IkenshoIkenshoInfoApplicantH18.java
@@ -19,7 +19,10 @@ public class IkenshoIkenshoInfoApplicantH18 extends IkenshoIkenshoInfoApplicant 
     }
   }
   private void jbInit() throws Exception {
-    getAgreeGroup().setText("主治医として、本意見書が介護サービス計画作成に利用されることに");
+// [ID:0000794][Satoshi Tokusari] 2015/3 edit-Start 意見書様式変更対応
+//    getAgreeGroup().setText("主治医として、本意見書が介護サービス計画作成に利用されることに");
+    getAgreeGroup().setText("主治医として、本意見書が介護サービス計画作成等に利用されることに");
+// [ID:0000794][Satoshi Tokusari] 2015/3 edit-End
 
   }
   
